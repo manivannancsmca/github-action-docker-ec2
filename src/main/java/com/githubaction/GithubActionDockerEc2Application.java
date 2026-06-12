81b24f8212b40d8070fd5eeb9d72971c165fe886
manivannancsmca <manivannancsmca@gmail.com>
2026-06-13 01:00:47 +0530

    first commit

---
+++ src/main/java/com/githubaction/GithubActionDockerEc2Application.java
@@ -0,0 +1,13 @@
+package com.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionDockerEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionDockerEc2Application.class, args);
+	}
+
+}
